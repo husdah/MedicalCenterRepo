82e46feb8e52a4e355801080eb3f2fa3d5b96625
ZeinabHijazi <hijazizeinab5@gmail.com>
2023-11-29 18:55:36 +0200

    aaa

--- assets/js/formValidationContact.js
+++ assets/js/formValidationContact.js
@@ -1,123 +1,176 @@
 const form          = document.getElementById('form2');
-const  nameError    = document.getElementById('name-error');
-const  emailError   = document.getElementById('email-error');
-const  subjectError = document.getElementById('subject-error');
-const  messageError = document.getElementById('message-error');
 
+const fname_input   = document.getElementById('fname');
+const fnameError    = document.getElementById('fname-error');
+const lname_input   = document.getElementById('lname');
+const lnameError    = document.getElementById('lname-error');
+const email_input   = document.getElementById('email');
+const emailError    = document.getElementById('email-error');
+const subject_input = document.getElementById('subject');
+const subjectError  = document.getElementById('subject-error');
+const message_input = document.getElementById('message');
+const messageError  = document.getElementById('message-error');
 
+const btnsend       = document.getElementById('btnSend');
+var errorDisplayed = false;
 
 form.addEventListener('submit', e => { 
-    //prevents the default form submission behavior 
     e.preventDefault();
     validateForm();
 });
 
-const name_input    = document.getElementById('name');
-const email_input   = document.getElementById('email');
-const subject_input = document.getElementById('subject');
-const message_input = document.getElementById('message');
-const btnsend       = document.getElementById('btnSend');
-
-name_input.addEventListener('input',validateName);
-email_input.addEventListener('keyup', validateEmail);
-message_input.addEventListener('keyup', validateMessage);
-subject_input.addEventListener('keyup', validateSubject);
-
-
-
-function validateName(){
-    const name = name_input.value;
-    if(name.length == 0){
-        nameError.innerHTML = 'Name is required';
-        return false;
-    }
-    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
-        nameError.innerHTML = 'Please, Enter Your Full Name';
-        return false;
-    }
-    nameError.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
-    return true;
-}
-function validateEmail(){
-    const email = email_input.value;
-    if(email.length == 0){
-        emailError.innerHTML = 'Email is required';
-        return false;
-    }
-    if(!email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)){
-        emailError.innerHTML = 'Email Invalid';
-        return false;
-    }
-    emailError.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
-    return true;
-}
-function validateSubject(){
-    const subject = subject_input.value;
-    if(subject.length == 0){
-        subjectError.innerHTML = 'Name is required';
-        return false;
-    }
-    if(!subject.match(/^[A-Za-z]+$/)){
-        emailError.innerHTML = 'Email Invalid';
-        return false;
-    }
-    subjectError.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
-    return true;
-}
-function validateMessage(){
-    const message = message_input.value;
-    const required = 30;
-    const left = required - message.length;
-
-    if(left>0){
-        messageError.innerHTML = left + ' more characters required';
-        return false;
-    }
-    messageError.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
-    return true;
-}
-
+    /*if(fnameValue === '' && lnameValue === '' && emailValue === '' && subjectValue === '' && messageValue === ''){
+        fnameError.innerHTML   = '<i class="fa-solid fa-triangle-exclamation"></i> Please Enter Your First Name.';
+        lnameError.innerHTML   = '<i class="fa-solid fa-triangle-exclamation"></i> Please Enter Your Last Name.';
+        emailError.innerHTML   = '<i class="fa-solid fa-triangle-exclamation"></i> Please Enter Your Email.';
+        subjectError.innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i> Please Enter Your Subject.';
+        messageError.innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i> Please Enter Your Message.';
+    }*/
 function validateForm(){
-    const name    = name_input.value;
-    const email   = email_input.value;
-    const subject = subject_input.value;
-    const message = message_input.value;
-    var errorMessage = "";
+    const fnameValue   = fname_input.value;
+    const lnameValue   = lname_input.value;
+    const emailValue   = email_input.value;
+    const subjectValue = subject_input.value;
+    const messageValue = message_input.value;
+    const message = '';
 
-    /*if(name == 0 || email == 0 || subject == 0 || message == 0){
-        errorMessage += "Please Fill the form";
+        if(fnameValue === ''){
+            fnameError.innerHTML   = '<i class="fa-solid fa-triangle-exclamation"></i> Please Enter Your First Name.';
+            
+        }
+        else{
+            if(validateFirstname){
+                const message = 'true';
+            }
+        }
+        if(lnameValue === ''){
+            lnameError.innerHTML   = '<i class="fa-solid fa-triangle-exclamation"></i> Please Enter Your Last Name.';
+        }
+        if(emailValue === ''){
+            emailError.innerHTML   = '<i class="fa-solid fa-triangle-exclamation"></i> Please Enter Your Email.';
+        }
+        if(subjectValue === ''){
+             subjectError.innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i> Please Enter Your Subject.';
+        }
+        if(messageValue === ''){
+            messageError.innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i> Please Enter Your Message.';
+        }
+    /*if(fnameValue === '' && lnameValue === '' && emailValue === '' && subjectValue === '' && messageValue === ''){
     }
-    else */if(name == 0){
-        errorMessage += "Please enter your name.";
-    }
-    else if(!validateName()){
-        errorMessage += "Invalid Name Format.";
-    }
-    else if(email == 0){
-        errorMessage += "Please enter your email.";
-    }
-    else if(!validateEmail()){
-        errorMessage += "Invalid Email Format.";
-    }
-    else if(subject == 0){
-        errorMessage += "Please enter your subject.";
-    }
-    else if(!validateSubject()){
-        errorMessage += "Invalid Subject Format.";
-    }
-    else if(message == 0){
-        errorMessage += "Please enter your message.";
-    }
-    else if(!validateMessage()){
-        errorMessage += "Invalid Message Format.";
-    }
+    else if(fnameValue === '' || lnameValue === '' || emailValue === '' || subjectValue === '' || messageValue === ''){
+            alert('Fill the empty field(s).');
+    }*/
+    /*else if(fnameValue != '' || lnameValue != '' || emailValue != '' || subjectValue != '' || messageValue != '') {
+            if(validateFirstname() || validateLastname() || validateEmail() || validateSubject() || validateMessage()){
+                alert('Successfully Submitted');
+            }
+    }*/
+}
+const validateNameStructure = (name) => {
+    return name.match(/^[a-zA-Z]{3,}$/);
+};
+const validateEmailStructure = (email) => {
+    return email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+};
+const validateSubjectStructure = (subject) => {
+    return subject.match(/^.{1,}$/);
+};
 
-
-    if (errorMessage !== "") {
-        alert(errorMessage);
-    } 
-    else{
-        // Both fields are filled, you can proceed with further actions (e.g., form submission)
-        alert("Form submitted successfully!");
+const checkFirstname = () => {
+    if(fname_input.value !== ''){
+        fnameError.innerHTML   = "";
     }
 }
+fname_input.addEventListener('input', checkFirstname);
+
+const checkLastname = () => {
+    if(lname_input.value !== ''){
+        lnameError.innerHTML   = "";
+    }
+}
+lname_input.addEventListener('input', checkLastname);
+
+const checkEmail = () => {
+    if(email_input.value !== ''){
+        emailError.innerHTML   = "";
+    }
+}
+email_input.addEventListener('input', checkEmail);
+
+const checkSubject = () => {
+    if(subject_input.value !== ''){
+        subjectError.innerHTML   = "";
+    }
+}
+subject_input.addEventListener('input', checkSubject);
+
+const validateMessage = () => {
+    if(message_input.value !== ''){
+        messageError.innerHTML   = "";
+    }
+    const message      = message_input.value;
+    const required     = 30;
+    const left         = required - message.length;
+    messageError.innerHTML = '';
+    if(left>0){
+        messageError.innerHTML = left + ' more characters required. <br> Please the message must be clear.';
+        return false;
+    }
+    else{
+        messageError.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
+        return true;
+    }
+}
+message_input.addEventListener('input', validateMessage);
+
+const validateFirstname = () => {
+    const fnameValue   = fname_input.value;
+    if(!validateNameStructure(fnameValue)){
+        fnameError.innerHTML   = '<i class="fa-solid fa-triangle-exclamation"></i> The name must be at least 3 letters.';
+        return false;
+    }
+    else{
+        fnameError.innerHTML   = '<i class="fa-regular fa-circle-check"></i>';
+        return true;
+    }
+}
+fname_input.addEventListener('focusout', validateFirstname);
+
+const validateLastname = () => {
+    const lnameValue   = lname_input.value;
+    if(!validateNameStructure(lnameValue)){
+        lnameError.innerHTML   = '<i class="fa-solid fa-triangle-exclamation"></i> The name must be at least 3 letters.';
+        return false;
+    }
+    else{
+        lnameError.innerHTML   = '<i class="fa-regular fa-circle-check"></i>';
+        return true;
+    }
+}
+lname_input.addEventListener('focusout', validateLastname);
+
+const validateEmail = () => {
+    const emailValue   = email_input.value;
+    if(!validateEmailStructure(emailValue)){
+        emailError.innerHTML   = '<i class="fa-solid fa-triangle-exclamation"></i> Email must have such a structure: example@hotmail.com';
+        return false;
+    }
+    else{
+        emailError.innerHTML   = '<i class="fa-regular fa-circle-check"></i>';
+        return true;
+    }
+}
+email_input.addEventListener('focusout', validateEmail);
+
+const validateSubject = () => {
+    const subjectValue   = subject_input.value;
+    if(!validateSubjectStructure(subjectValue)){
+        subjectError.innerHTML   = '<i class="fa-solid fa-triangle-exclamation"></i> Subject must be clear.';
+        return false;
+    }
+    else{
+        subjectError.innerHTML   = '<i class="fa-regular fa-circle-check"></i>';
+        return true;
+    }
+}
+subject_input.addEventListener('focusout', validateSubject);
