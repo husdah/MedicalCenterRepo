db97d5cfe8333de82d5f77b636d50025dd8264f2
hayatfaily <haya.toufayli.2020@outlook.com>
2023-12-13 00:22:05 +0200

    shelet l comment aan l trigger!

--- clinics.php
+++ clinics.php
@@ -1,3 +1,8 @@
+<?php
+require('config/dbcon.php');
+$query="select clinicId,name,description,icon from clinic";
+$res=mysqli_query($con,$query);
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -31,19 +36,26 @@
       </div>
       <div class="clinicsContainer">
       <div class="clinics">
-        <div class="item">
-          <a href="bookappsinglenew.php">
-            <!-- <i class="fa-solid fa-heart-pulse"></i> -->
-            <img src="images/heart2.png" alt="">
-            <div class="name"> Cardiology</div>
+<?php 
+while($row=mysqli_fetch_assoc($res))
+{
+  echo '
+       <div class="item">
+          <a href="bookappsinglenew.php?cid='.$row["clinicId"].'">
+            <img src="'.$row["icon"].'" alt="">
+            <div class="name">'.$row["name"].'</div>
             <div class="des">
-              Lorem ipsum dolor sit amet consectetur adipisicing e
+              '.$row["description"].'
             </div>
           </a>
         </div>
-        <div class="item">
+  ';
+}
+?>
+
+        <!-- <div class="item">
           <a href="bookappsinglenew.php">
-            <!-- <i class="fa-solid fa-brain"></i> -->
+            <i class="fa-solid fa-brain"></i>
             <img src="images/brain2.png" alt="">
             <div class="name"> Neurology</div>
             <div class="des">
@@ -53,7 +65,7 @@
         </div>
         <div class="item">
           <a href="bookappsinglenew.php">
-            <!-- <i class="fa-regular fa-eye"></i> -->
+            <i class="fa-regular fa-eye"></i>
             <img src="images/eye-regular.svg" alt="">
             <div class="name"> Ophtalmology</div>
             <div class="des">
@@ -63,7 +75,7 @@
         </div>
         <div class="item">
           <a href="bookappsinglenew.php">
-            <!-- <i class="fa-solid fa-heart-pulse"></i> -->
+            <i class="fa-solid fa-heart-pulse"></i>
             <img src="images/otorhinolaryngology2.png" alt="">
             <div class="name"> Cardiology</div>
             <div class="des">
@@ -73,7 +85,7 @@
         </div>
         <div class="item">
           <a href="bookappsinglenew.php">
-            <!-- <i class="fa-solid fa-brain"></i> -->
+            <i class="fa-solid fa-brain"></i>
             <img src="images/dermatology2.png" alt="">
             <div class="name"> Neurology</div>
             <div class="des">
@@ -83,7 +95,7 @@
         </div>
         <div class="item">
           <a href="bookappsinglenew.php">
-            <!-- <i class="fa-regular fa-eye"></i> -->
+            <i class="fa-regular fa-eye"></i>
             <img src="images/surgery2.png" alt="">
             <div class="name"> Ophtalmology</div>
             <div class="des">
@@ -93,7 +105,7 @@
         </div>
         <div class="item">
           <a href="bookappsinglenew.php">
-            <!-- <i class="fa-solid fa-heart-pulse"></i> -->
+            <i class="fa-solid fa-heart-pulse"></i>
             <img src="images/radiology2.png" alt="">
             <div class="name"> Cardiology</div>
             <div class="des">
@@ -103,7 +115,7 @@
         </div>
         <div class="item">
           <a href="bookappsinglenew.php">
-            <!-- <i class="fa-solid fa-brain"></i> -->
+            <i class="fa-solid fa-brain"></i>
             <img src="images/dental2.png" alt="">
             <div class="name"> Neurology</div>
             <div class="des">
@@ -113,7 +125,7 @@
         </div>
         <div class="item">
           <a href="bookappsinglenew.php">
-            <!-- <i class="fa-regular fa-eye"></i> -->
+            <i class="fa-regular fa-eye"></i>
             <img src="images/stomach2.png" alt="">
             <div class="name"> Ophtalmology</div>
             <div class="des">
@@ -123,7 +135,7 @@
         </div>
         <div class="item">
           <a href="bookappsinglenew.php">
-            <!-- <i class="fa-solid fa-brain"></i> -->
+            <i class="fa-solid fa-brain"></i>
             <img src="images/orthopedic2.png" alt="">
             <div class="name"> Neurology</div>
             <div class="des">
@@ -148,7 +160,7 @@
               Lorem ipsum dolor sit amet consectetur adipisicing e
             </div>
           </a>
-        </div>
+        </div> -->
       </div>
     </div>
     </div>
