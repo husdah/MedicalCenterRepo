deaee00bb5afe147c1a940190353531a936411fd
ZeinabHijazi <hijazizeinab5@gmail.com>
2023-12-16 13:54:21 +0200

    aaa

--- sendEmail.php
+++ sendEmail.php
@@ -9,7 +9,8 @@
     use PHPMailer\PHPMailer\Exception;
 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $name    = $_POST['fname'].$_POST['lname'];
+        echo "<script>alert('clicked')</script>";
+        /*$name    = $_POST['fname'].$_POST['lname'];
         $email   = $_POST['email'];
         $subject = $_POST['subject'];
         $message = $_POST['message'];
@@ -22,11 +23,11 @@
             $mail->SMTPAuth = true;//enable smtp authentication
             $mail->SMTPSecure = 'tls';//set type of encryption 
             $mail->Port = 587;//set port to connect smtp
-            $mail->Username = "";//set gmail username    
-            $mail->Password = "";//set gmail password
+            $mail->Username = "healthhubcenter23@gmail.com";//set gmail username    
+            $mail->Password = "hhCenter@23";//set gmail password
             
-            $mail->setFrom('hijazizeinab5@gmail.com', 'zeinab hijazi');
-            $mail->addAddress("hijazizeinab5@gmail.com");//add recipient
+            $mail->setFrom('healthhubcenter23@gmail.com', 'Admin');
+            $mail->addAddress("healthhubcenter23@gmail.com");//add recipient
            
             $mail->isHTML(true);//set sender email
            
@@ -38,6 +39,6 @@
         }
         catch(Exception $e){
             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        }
+        }*/
     }
 ?>
