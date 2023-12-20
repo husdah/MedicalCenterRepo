16d45181e8ff8048e1d1e38a80c839bb48b5b44a
ZeinabHijazi <hijazizeinab5@gmail.com>
2023-12-20 21:56:17 +0200

    finish user.php page

--- getPatientData.php
+++ getPatientData.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     $userId = 10; //$userId = $_SESSION['userId'];
+    header('Content-type: application/json');
     
     class user{
         public $id;
@@ -8,16 +9,7 @@
         public $date;
         public $time;
     }
-    class patient{
-        public $firstName;
-        public $lastName;
-        public $email;
-        public $phoneNumber;
-        public $date;
-        //public $gender;
-        public $bloodType;
-    }
-
+    
     require_once('config/dbcon.php');
     $query = 'SELECT
 	                appointment.appID AS appointmentID, concat(user.Fname, " ", user.Lname) AS doctor, appointment.date, appointment.time, patient.patientId, doctor.doctorId
@@ -38,7 +30,7 @@
         mysqli_stmt_execute($stmt);
         $result = mysqli_stmt_get_result($stmt);
         if(mysqli_num_rows($result) > 0){
-            $data = [];
+            $patinetApp = [];
             // output data of each row
             for ($i = 0; $row = $result->fetch_assoc(); $i++) {
                 $user = new user();
@@ -46,50 +38,13 @@
                 $user-> doctor = $row['doctor'];
                 $user-> date   = $row['date'];
                 $user-> time   = $row['time'];
-                array_push($data, $user);
+                array_push($patinetApp, $user);
             }
         }
     } 
     else {
         die("Error in prepared statement: " . mysqli_error($con));
     }
-
-    /*$select_patient_data = 'SELECT  
-                                user.fname, user.lname, user.email, patient.phoneNumber, patient.dateOfBirth, patient.gender, patient.bloodType
-                            FROM 
-                                user
-                            JOIN
-                                patient ON user.userId = patient.userId 
-                            WHERE
-                                user.userId = ?; 
-                            ';
-    $stmt2 = mysqli_prepare($con, $select_patient_data);
-    if ($stmt2) {
-        mysqli_stmt_bind_param($stmt2, "i", $userId);
-        mysqli_stmt_execute($stmt2);
-        $result2 = mysqli_stmt_get_result($stmt2);
-        if(mysqli_num_rows($result2) > 0){
-            $data2 = [];
-            // output data of each row
-            for ($i = 0; $row = $result2->fetch_assoc(); $i++) {
-                $patient = new patient();
-                $patient-> firstName   = $row['fname'];
-                $patient-> lastName    = $row['lname'];
-                $patient-> email       = $row['email'];
-                $patient-> phoneNumber = $row['phoneNumber'];
-                $patient-> date        = $row['dateOfBirth'];
-                //$patient-> gender      = $row['gender'];
-                $patient-> bloodType   = $row['bloodType'];
-                array_push($data2, $patient);
-            }
-        }
-    } 
-    else {
-        die("Error in prepared statement: " . mysqli_error($con));
-    }
-    // $data["sub_count"] = $result->num_rows;
-    // $conn->close();
-    echo json_encode($data2);*/
-    echo json_encode($data);
+    echo json_encode($patinetApp);
 
 ?>
